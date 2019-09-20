8bcf76bb8e8e51802d8fe09f9188e3653385ef28
LDwhoRU <50721703+LDwhoRU@users.noreply.github.com>
2019-09-21 01:58:29 +1000

    Major shift
    Added Login and Register, fixed case usage in routes (Use lowercase) and fixed CSS for home page that was overwritten.

--- bench/routes.py
+++ bench/routes.py
@@ -10,9 +10,15 @@ def newListing():
 @app.route('/manage')
 def manageListing():
     return render_template('manageListing.html')
-@app.route('/Listing')
+@app.route('/listing')
 def viewListing():
     return render_template("view.html")
+@app.route('/register')
+def register():
+    return render_template("register.html")
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 @app.errorhandler(404)
 def not_found(e):
