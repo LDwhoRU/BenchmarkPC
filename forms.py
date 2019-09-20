af3c159c85b61a21f0271aeba6cbb8820a276f64
brock-1991 <55529622+brock-1991@users.noreply.github.com>
2019-09-20 22:05:47 +1000

    Python classes for login and register forms

---
+++ forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
+from wtforms.validators import InputRequired, Length, Email, EqualTo
+
+class LoginForm(FlaskForm):
+    user_name = StringField("User Name", validators=[InputRequired('Enter user name')])
+    password = PasswordField("Password", validators=[InputRequired('Enter Password')])
+    submit = SubmitField("Login")
+
+class RegisterForm(FlaskForm):
+    user_name = StringField("User Name", validators=[InputRequired()])
+    email_id = StringField("Email Address", validators=[InputRequired("Please enter a valid email address")])
+    password = PasswordField("Password", validators=[InputRequired(), EqualTo('confirm', message="Passwords should match")])
+    confirm = PasswordField("Confirm Password")
+    submit = SubmitField("Register")
+
+
