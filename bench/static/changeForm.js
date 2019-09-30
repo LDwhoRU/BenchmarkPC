6d6de5eb6a82508576770c85fb07cb4322ea8a3c
PhantomTiger <sthephantomtiger@gmail.com>
2019-09-30 19:23:44 +1000

    Finished CPU Cooler Form

--- bench/static/changeForm.js
+++ bench/static/changeForm.js
@@ -7,8 +7,8 @@ function myFunction(){
     var value = select.options[select.selectedIndex].value;
     console.log(value);
     clearSpecifications();
-    if(value == "CPU"){
-        getNewText();
+    if(value != "default"){
+        getNewText(value);
     }
 }
 
@@ -17,13 +17,23 @@ function clearSpecifications(){
     table.innerHTML = "";
 }
 
-function getNewText(){
+function getNewText(value){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
         document.getElementById("table").innerHTML = this.responseText;
         }
     };
-    xhttp.open("GET", "static/CPU html.txt", true);
+    switch(value){
+        case "CPU":
+            xhttp.open("GET", "static/CPU html.html", true);
+            break;
+        case "CPU Cooler":
+            xhttp.open("GET", "static/CPUCoolerHTML .html", true);
+            break;
+
+
+
+    }
     xhttp.send();
 }
