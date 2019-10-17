dd79d452e09d54deefc242c0f313239fcc34b0e0
unknown <sthephantomtiger@gmail.com>
2019-10-17 12:35:20 +1000

    Added Benchmark

--- bench/static/javascript/cpu_benchmark.js
+++ bench/static/javascript/cpu_benchmark.js
@@ -1,10 +1,10 @@
 
-
+let activated = false;
 function bench() {
     let product = 1.0;
     for (let counter = 1; counter < 1000; counter++) {
         for (let dex = 1; dex < 360; dex++) {
-            angle = Math.radians(dex);
+            angle = dex * (Math.PI / 180);
             product *= Math.pow(Math.sin(angle), 2) + Math.pow(Math.cos(angle), 2);
         }
     }
@@ -14,14 +14,21 @@ function timeIt(repeats, functionName) {
     let times = [];
     for (let i = 0; i < repeats; i++) {
         let t0 = performance.now();
-        functionName();
+        for (let j = 0; j < 10; j++) {
+            functionName();
+        }
         let t1 = performance.now();
-        times.push(t1 - t0);
+        times.push((t1 - t0));
     }
     return times;
 }
+function main() {
+    if (!activated) {
+        result = timeIt(10, bench);
+        result.sort((a, b) => a - b);
+        final_result = Math.floor(((3 - (result[0] / 1000))) * (1 / 8) * 100);
+        console.log(final_result);
+        activated = true;
+    }
+}
 
-result = timeit.repeat(10, bench);
-result.sort((a, b) => b - a);
-final_result = (3 - result[0] / 1.8) * 100;
-console.log(final_result);
