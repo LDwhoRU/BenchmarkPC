eb2c71f50d3993b072d4ef6396101439a5ceaaa1
Brock Barker <b.bark1991@gmail.com>
2019-10-21 12:55:57 +1000

    created route for current listings page

--- bench/routes.py
+++ bench/routes.py
@@ -256,16 +256,19 @@ def logout():
     logout_user()
     return redirect("/")
 
-<<<<<<< Updated upstream
 @app.route('/search')
 def search():
     return render_template("search.html")
-=======
+
 @app.route('/history')
 def history():
     if current_user.is_anonymous:
         return redirect('/login')
-    title = "Past Sale | BenchmarkPC"
+    title = "Past Sales | BenchmarkPC"
     return render_template("history.html")
->>>>>>> Stashed changes
+
+@app.route('/currentListings')
+def current_listings():
+    title = "Current Listings | BenchmarkPC"
+    return render_template("currentListings.html")
 
