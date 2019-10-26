e251019b0c4f1f6d6f8dd535ade5b32d832974e0
unknown <sthephantomtiger@gmail.com>
2019-10-26 20:12:57 +1000

    Got the actual user object

--- bench/routes.py
+++ bench/routes.py
@@ -360,7 +360,7 @@ def register():
             db.session.add(user)
             db.session.commit()
 
-            user = User.query.filter_by(id=user.id)
+            user = User.query.filter_by(id=user.id).first()
             login_user(user)
             return redirect("/")
         else:
