d03f0c88cfe5b0a680a5d73591552321c9f0bca8
unknown <sthephantomtiger@gmail.com>
2019-10-26 19:26:25 +1000

    Made username not unique

--- bench/models.py
+++ bench/models.py
@@ -8,7 +8,7 @@ from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     phone = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
